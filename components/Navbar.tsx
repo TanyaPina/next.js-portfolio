c6bbaad8fdf67766ffb561e3918c23ef1283b934
TanyaPina <tanyapina15@gmail.com>
2023-06-28 02:53:48 -0700

    added imports to navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import { Link } from 'react-scroll'
+import { useTheme } from 'next-themes'
+import { RiMoonFill, RiSunLine } from 'react-icons/ri'
+import { IoMdMenu, IoMdClose } from 'react-icons/io'
 
 const Navbar = () => {
   return (
