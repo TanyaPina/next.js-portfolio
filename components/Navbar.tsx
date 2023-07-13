30abdc67c9f91e6555d3e0788710944deff00665
TanyaPina <tanyapina15@gmail.com>
2023-07-13 14:38:29 -0700

    added hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,21 +28,47 @@ const NAV_ITEMS: Array<NavItem> = [
 const Navbar = () => {
 
   const { systemTheme, theme, setTheme } = useTheme()
-  const currentTheme = theme ===  "system" ? systemTheme : theme
+  const currentTheme = theme === "system" ? systemTheme : theme
   const [navbar, setNavbar] = useState(false)
 
   return (
-  <header className="w-full mx-auto px-4 bg-white shadow fixed top-0 z-50 dark:bg-stone-900 dark:border-b dark:border-stone-600">
-    <div className="justify-between md:items-center md:flex">
-      <div>
-        <div className="md:py-5 md:block">
-          <h2 className="text-2xl font-bold ">Tanya Piña</h2>
+    <header className="w-full mx-auto px-4 bg-white shadow fixed top-0 z-50 sm:px-20 dark:bg-stone-900 dark:border-b dark:border-stone-600">
+      <div className="justify-between md:items-center md:flex">
+        <div>
+          <div className="flex items-center justify-between py-3">
+            <div className="md:py-5 md:block">
+              <h2 className="text-2xl font-bold ">Tanya Piña</h2>
+            </div>
+            <div className="md:hidden">
+              <button onClick={() => setNavbar(!navbar)}>
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="md:flex md:space-x-6">
-        {NAV_ITEMS.map((item, idx) => {
-          return <a key={idx}>{item.label}</a>
-        })}
+        <div>
+        <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}>
+          <div className="items-center justify-center md:flex md:space-x-6">
+            {NAV_ITEMS.map((item, idx) => {
+              return (<Link
+                key={idx}
+                to={item.page}
+                className={
+                  "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                }
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                onClick={() => setNavbar(!navbar)}
+              >
+                {item.label}
+              </Link>
+            )
+          })}
             {
               currentTheme === "dark" ? (
                 <button onClick={() => setTheme("light")}>
@@ -50,12 +76,14 @@ const Navbar = () => {
                 </button>
               ) : (
                 <button onClick={() => setTheme("dark")}>
-                  <RiMoonFill size={25}/>
+                  <RiMoonFill size={25} />
                 </button>
               )}
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
   )
 }
 
