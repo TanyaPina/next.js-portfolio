ce2940b6ce123fd9b2ab04018c0c7fe9b15d93f8
TanyaPina <tanyapina15@gmail.com>
2023-07-22 16:41:10 -0700

    initialized about me section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection"
+import AboutMeSection from "@/components/AboutMeSection"
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <HeroSection/>
+      <AboutMeSection/>
     </main>
   )
 }
---
+++ components/AboutMeSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AboutMeSection = () => {
+  return (
+    <div>AboutMeSection</div>
+  )
+}
+
+export default AboutMeSection
