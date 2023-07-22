28684103a977ce085e7c92c170ec7c084a37b4cb
TanyaPina <tanyapina15@gmail.com>
2023-07-21 21:08:17 -0700

    added headshot image

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
   return (
-    <section id="home">HeroSection</section>
+    <section id="home">
+      <div>
+        <div>
+          <Image src="/headshot.jpg" 
+          alt="Headshot of Tanya, a Mexican-American person wearing a smile and a burgundy shirt." 
+          width={300} 
+          height={300}/>
+        </div>
+        <div>
+        </div>
+      </div>
+    </section>
   )
 }
 
