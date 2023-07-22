703569c10fe3d2c1bc206ba266504f5086479d99
TanyaPina <tanyapina15@gmail.com>
2023-07-21 20:36:43 -0700

    added styling to page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/HeroSection"
 
 export default function Home() {
   return (
-    <main>
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <HeroSection/>
     </main>
   )
