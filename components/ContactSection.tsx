478775580ec2f2e6c17e949b1be88399807d91c9
TanyaPina <tanyapina15@gmail.com>
2023-07-23 13:48:01 -0700

    added header

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const ContactSection = () => {
-  return (
-    <div>ContactSection</div>
-  )
+    return (
+        <section id="contact">
+            <hr className="w-6 h-1 mx-auto my-10 bg-teal-500 border-0 rounded"></hr>
+            <h2 className="text-center text-2xl font-bold mb-10">Contact Me</h2>
+        </section>
+    )
 }
 
 export default ContactSection
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import {
-  AiFillHeart,
-} from "react-icons/ai"
+import { AiFillHeart } from "react-icons/ai"
 
 const Footer = () => {
   return (
