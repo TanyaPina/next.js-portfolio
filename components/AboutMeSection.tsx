4f274dbb6c3ba5cd1b1fed260dcb4e7655fa581d
TanyaPina <tanyapina15@gmail.com>
2023-07-22 18:55:37 -0700

    added placeholder text

--- components/AboutMeSection.tsx
+++ components/AboutMeSection.tsx
@@ -5,6 +5,27 @@ export const AboutMeSection = () => {
     <section id="about">
         <div>
             <h1>About Me</h1>
+            <p>
+            Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws 
+            please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you 
+            rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean 
+            to me please let me back inside.
+            </p>
+            <br/>
+            <p>
+            Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws 
+            please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you 
+            rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean 
+            to me please let me back inside.
+            </p>
+            <br/>
+            <p>
+            Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws 
+            please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you 
+            rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean 
+            to me please let me back inside.
+            </p>
+            <br/>
         </div>
     </section>
   )
