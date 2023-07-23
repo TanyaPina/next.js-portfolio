0fbc1141fc68970695888bbf5a7c41e961eeb720
TanyaPina <tanyapina15@gmail.com>
2023-07-23 16:04:10 -0700

    changed apostrophes

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
             height={300} />
         </div>
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl"> Hi, I'm Tanya!</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl"> Hi, I&apos;m Tanya!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&apos;m a {" "}
             <span className="font-semibold text-teal-800 dark:text-teal-500">
