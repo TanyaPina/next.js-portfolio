924c25ea27d2bbabaa52a069b3be278f6a1e5fa2
TanyaPina <tanyapina15@gmail.com>
2023-07-23 12:48:59 -0700

    added heart icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {
+  AiFillHeart,
   AiOutlineGithub,
   AiOutlineTwitter,
   AiOutlineLinkedin,
@@ -10,6 +11,12 @@ const Footer = () => {
   return (
     <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
+      <div className="">
+        <div className="flex flex-row items-center justify-center space-x-1">
+          Made with <AiFillHeart className="mr-2 ml-2"/> by Tanya Piña
+          </div>
+        <div></div>
+      </div>
     </footer>
   )
 }
