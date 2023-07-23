ad5683177238729106abc9ccf8e83b1df89ebf7e
TanyaPina <tanyapina15@gmail.com>
2023-07-22 19:22:26 -0700

    addded styling to background story section

--- components/AboutMeSection.tsx
+++ components/AboutMeSection.tsx
@@ -1,34 +1,42 @@
 import React from 'react'
 
 export const AboutMeSection = () => {
-  return (
-    <section id="about">
-        <div>
-            <h1>About Me</h1>
-            <p>
-            Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws 
-            please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you 
-            rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean 
-            to me please let me back inside.
-            </p>
-            <br/>
-            <p>
-            Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws 
-            please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you 
-            rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean 
-            to me please let me back inside.
-            </p>
-            <br/>
-            <p>
-            Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws 
-            please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you 
-            rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean 
-            to me please let me back inside.
-            </p>
-            <br/>
-        </div>
-    </section>
-  )
+    return (
+        <section id="about">
+            <div className="my-12 pb-12 md:pt-16 md:pb-48">
+                <h2 className="text-center font-bold text-3xl">About Me</h2>
+                <div className="flex flex-col space-y-10 item-stretch justify-center align-top md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10">
+                    <div className="md:w-1/2">-
+                    
+                    <p>
+                        Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws
+                        please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you
+                        rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean
+                        to me please let me back inside.
+                    </p>
+                    <br />
+                    <p>
+                        Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws
+                        please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you
+                        rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean
+                        to me please let me back inside.
+                    </p>
+                    <br />
+                    <p>
+                        Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws
+                        please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you
+                        rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean
+                        to me please let me back inside.
+                    </p>
+                    <br />
+                    </div>
+                </div>
+                <div className="md:w-1/2 text-center md:text-left">
+                    <h2>My Skills</h2>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default AboutMeSection
