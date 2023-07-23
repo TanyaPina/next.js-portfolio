4b0707f8a5fb7d423519ece235957427635bc6c7
TanyaPina <tanyapina15@gmail.com>
2023-07-22 22:20:02 -0700

    added header

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const ProjectsSection = () => {
   return (
-    <section id="projects">ProjectsSection</section>
+    <section id="projects">
+    <hr className="w-6 h-1 mx-auto my-10 bg-teal-500 border-0 rounded"></hr>
+    <h1 className="text-center text-2xl font-bold ">Projects</h1>
+    </section>
   )
 }
 
