cef98b11ca6a8c93d8212e8890412604b3e6cc59
TanyaPina <tanyapina15@gmail.com>
2023-07-22 19:31:06 -0700

    created skills object

--- components/AboutMeSection.tsx
+++ components/AboutMeSection.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 
+const skills = [
+    { skill: "HTML" },
+    { skill: "CSS" },
+    { skill: "JavaScript" },
+    { skill: "React" },
+    { skill: "Express" },
+    { skill: "Node.js" },
+    { skill: "PostgreSQL" },
+    { skill: "Git" },
+    { skill: "GitHub" },
+    { skill: "Postman" },
+    { skill: "Vite" },
+    { skill: "Material UI" },
+    { skill: "Semantic UI" },
+    { skill: "Daisy UI" },
+]
+
 export const AboutMeSection = () => {
     return (
         <section id="about">
@@ -7,28 +24,28 @@ export const AboutMeSection = () => {
                 <h2 className="text-center font-bold text-3xl">About Me</h2>
                 <div className="flex flex-col space-y-10 item-stretch justify-center align-top md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10">
                     <div className="md:w-1/2">-
-                    
-                    <p>
-                        Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws
-                        please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you
-                        rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean
-                        to me please let me back inside.
-                    </p>
-                    <br />
-                    <p>
-                        Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws
-                        please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you
-                        rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean
-                        to me please let me back inside.
-                    </p>
-                    <br />
-                    <p>
-                        Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws
-                        please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you
-                        rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean
-                        to me please let me back inside.
-                    </p>
-                    <br />
+
+                        <p>
+                            Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws
+                            please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you
+                            rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean
+                            to me please let me back inside.
+                        </p>
+                        <br />
+                        <p>
+                            Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws
+                            please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you
+                            rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean
+                            to me please let me back inside.
+                        </p>
+                        <br />
+                        <p>
+                            Nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws
+                            please let me outside pouty face yay! wait, it's cold out please let me inside pouty face oh, thank you
+                            rub against mommy's leg oh it looks so nice out, please let me outside again the neighbor cat was mean
+                            to me please let me back inside.
+                        </p>
+                        <br />
                     </div>
                 </div>
                 <div className="md:w-1/2 text-center md:text-left">
