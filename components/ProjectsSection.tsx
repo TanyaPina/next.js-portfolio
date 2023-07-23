c3a77c403fa8d02746fcfbe61846cece23c24e60
TanyaPina <tanyapina15@gmail.com>
2023-07-22 23:14:47 -0700

    added project descriptions

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -5,48 +5,53 @@ import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
 
 const projects = [
     {
-      name: "Guess-a-Mon",
-      description:
-        "Guess-a-Mon is a web app that uses the PERN stack to provide a way for the nostalgic anime fans to test themselves on their Pokémon knowledge.",
-      image: "/Guess-a-Mon.png",
-      github: "https://github.com/TanyaPina/guess-a-mon",
-      link: "server-stus.onrender.com/",
+        name: "Guess-a-Mon",
+        description:
+            "Guess-a-Mon is a web app that uses the PERN stack to provide a way for the nostalgic anime fans to test themselves on their Pokémon knowledge.",
+        image: "/Guess-a-Mon.png",
+        github: "https://github.com/TanyaPina/guess-a-mon",
+        link: "server-stus.onrender.com/",
     },
     {
-      name: "Question Craft",
-      description: "Question-Craft is a group-made web application that generates questions using the Chat GPT OpenAI API based on user input. It utilizes React and Bootstrap for the frontend and Node.js with Express for the backend.",
-      image: "/Question Craft.png",
-      github: "https://github.com/TanyaPina/question-craft",
+        name: "Question Craft",
+        description: "Question-Craft is a group-made web application that generates questions using the Chat GPT OpenAI API based on user input. It utilizes React and Bootstrap for the frontend and Node.js with Express for the backend.",
+        image: "/Question Craft.png",
+        github: "https://github.com/TanyaPina/question-craft",
     },
-  ]
+]
 
 const ProjectsSection = () => {
-  return (
-    <section id="projects">
-    <hr className="w-6 h-1 mx-auto my-10 bg-teal-500 border-0 rounded"></hr>
-    <h1 className="text-center text-2xl font-bold ">Projects</h1>
-    <div className="flex flex-col space-y-28">
-        {projects.map((project, idx) => {
-          return (
-            <div key={idx}>
-                <div className="flex flex-col md: space-x-12">
-                    <div className="md:w-1/2">
-                        <Image
-                        src={project.image}
-                        alt=""
-                        width={1000}
-                        height={1000}
-                        className="rounded-xl shadow-xl hover:opacity-70"
-                        />
-                    </div>
-                    <div></div>
-                </div>
+    return (
+        <section id="projects">
+            <hr className="w-6 h-1 mx-auto my-10 bg-teal-500 border-0 rounded"></hr>
+            <h2 className="text-center text-2xl font-bold mb-6">Projects</h2>
+            <div className="flex flex-col space-y-28">
+                {projects.map((project, idx) => {
+                    return (
+                        <div key={idx}>
+                            <div className="flex flex-col md:space-x-12">
+                                <div className="md:w-1/2">
+                                    <Image
+                                        src={project.image}
+                                        alt=""
+                                        width={1000}
+                                        height={1000}
+                                        className="rounded-xl shadow-xl hover:opacity-70"
+                                    />
+                                </div>
+                                <div className="md:w-1/2">
+                                    <h2>{project.name}</h2>
+                                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                                        {project.description}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
-            )
-        })}   
-      </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default ProjectsSection
